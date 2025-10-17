3277ac2401d50b44b57e2fc95cb740b80650406e
JakeQusha <mateuszjakoniuk@outlook.com>
2025-10-17 16:32:55 +0200

    big mommy

--- casino/aplikacja/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LeCasinoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'casino.aplikacja'
--- casino/settings.py
+++ casino/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'casino.aplikacja.apps.LeCasinoConfig',
+    'casino.login',
+    'casino.roulette'
 ]
 
 MIDDLEWARE = [
