8089330e3d637adf9ac26286d78a54f77c37b7dc
JakeQusha <mateuszjakoniuk@outlook.com>
2025-10-18 16:45:10 +0200

    quality of gamble

--- casino/roulette/views.py
+++ casino/roulette/views.py
@@ -19,8 +19,10 @@ def roulette(request):
             choice = int(request.POST.get("choice"))
             quantity = int(request.POST.get("quantity"))
         except Exception:
-            err = "Invalid Ammount"
+            err = "Invalid Amount"
         if err is None:
+            request.session["bet"] = quantity
+            request.session["choice"] = choice
             if quantity > user.balance or user.balance <= 0:
                 result = 2
             if result != 2 :
@@ -32,4 +34,4 @@ def roulette(request):
                     result = 0
                     user.balance -= quantity
                 user.save()
-    return render(request, "casino/roulette/index.html", {"balance": user.balance, "result": result, "error": err})
+    return render(request, "casino/roulette/index.html", {"balance": user.balance, "result": result, "error": err, "last_bet": request.session["bet"], "last_choice": request.session["choice"]})
