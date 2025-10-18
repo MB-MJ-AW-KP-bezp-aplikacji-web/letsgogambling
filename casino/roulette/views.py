536e21e007c7ea1d3bb8ae709bd7889a5692d6f0
JakeQusha <mateuszjakoniuk@outlook.com>
2025-10-18 16:27:28 +0200

    mniej dzuir

--- casino/roulette/views.py
+++ casino/roulette/views.py
@@ -10,19 +10,26 @@ from casino.login.models import User
 @login_required(login_url='/login/')
 def roulette(request):
     result = None
+    err = None
     user = request.user
     if request.method == "POST":
-        choice = request.POST.get("choice")
-        quantity = int(request.POST.get("quantity"))
-        if int(quantity) > user.balance or user.balance <= 0:
-            result = 2
-        if choice and result != 2 :
-            rand = random.randint(0, 1)
-            if rand == int(choice):
-                result = 1
-                user.balance += quantity
-            else:
-                result = 0
-                user.balance -= quantity
-            user.save()
-    return render(request, "casino/roulette/index.html", {"balance": user.balance, "result": result})
+        choice = None
+        quantity = None
+        try:
+            choice = int(request.POST.get("choice"))
+            quantity = int(request.POST.get("quantity"))
+        except Exception:
+            err = "Invalid Ammount"
+        if err is None:
+            if quantity > user.balance or user.balance <= 0:
+                result = 2
+            if result != 2 :
+                rand = random.randint(0, 1)
+                if rand == choice:
+                    result = 1
+                    user.balance += quantity
+                else:
+                    result = 0
+                    user.balance -= quantity
+                user.save()
+    return render(request, "casino/roulette/index.html", {"balance": user.balance, "result": result, "error": err})
