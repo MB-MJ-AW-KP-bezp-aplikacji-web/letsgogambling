7cca839b92ff162252cb5e61594744138c0684fb
adam <adamwwsluzbowy@gmail.com>
2025-10-18 17:05:05 +0200

    challenge added

--- casino/user_mgr/views.py
+++ casino/user_mgr/views.py
@@ -12,6 +12,7 @@ def profile_page(request):
     }
     return render(request, "casino/user_mgr/index.html", miner_data)
 
+@login_required(login_url='/login/')
 def add_money(request):
     user = request.user
     miner_data = {
@@ -30,4 +31,12 @@ def magic_money_button(request):
         messages.success(request, "Updated balance! +$100")
         return redirect('add_money')
     messages.error(request, "Donation error")
+    return redirect('add_money')
+
+@login_required(login_url='/login/')
+def challenge(request):
+    user = request.user
+    if request.method == "POST":
+        amount = int(request.POST["quantity"])
+        return redirect('add_money')
     return redirect('add_money')
