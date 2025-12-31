dc64c377a9af683f1c834aaf7741b085837152a2
JakeQusha <mateuszjakoniuk@outlook.com>
2025-12-31 01:09:51 +0100

    fix linter humours

--- casino/asgi.py
+++ casino/asgi.py
@@ -12,12 +12,12 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
+from casino.roulette.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'casino.settings')
 
 django_asgi_app = get_asgi_application()
 
-from casino.roulette.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
