47debcb902618873106394bcb37a2e98ca8a5c8a
JakeQusha <mateuszjakoniuk@outlook.com>
2025-12-31 14:40:33 +0100

    cosmetic stuff

--- casino/api/urls.py
+++ casino/api/urls.py
@@ -4,6 +4,7 @@ import casino.api.views as v
 urlpatterns = [
     path("", v.ListCreate.as_view(), name="view_create_view"),
     path("spin/", v.spin_api, name="spin_api"),
+    path("coinflip/", v.coinflip_api, name="coinflip_api"),
     path("balance/", v.get_balance, name="my_balance")
     # path("l/", v.login_user, name="login")
 ]
