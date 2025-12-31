61a39982588f23c864777d4b3a76e4d4b12b78ab
JakeQusha <mateuszjakoniuk@outlook.com>
2025-12-31 01:19:39 +0100

    fix formatting

--- casino/coinflip/views.py
+++ casino/coinflip/views.py
@@ -21,7 +21,7 @@ def coinflip(request):
             request.session["choice"] = usr_choice
             if quantity > user.balance or user.balance <= 0:
                 result = 2
-            if result != 2 :
+            if result != 2:
                 rand = choice([0, 1])
                 if rand == usr_choice:
                     result = 1
@@ -31,4 +31,6 @@ def coinflip(request):
                     user.balance -= quantity
                 user.save()
     bet = request.session.get("bet")
-    return render(request, "casino/coinflip/index.html", {"balance": user.balance, "result": result, "error": err, "last_bet": bet if bet is not None else 10, "last_choice": request.session.get("choice")})
+    return render(request, "casino/coinflip/index.html",
+                  {"balance": user.balance, "result": result, "error": err, "last_bet": bet if bet is not None else 10,
+                   "last_choice": request.session.get("choice")})
